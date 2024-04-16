b12f698222a17aad034a6cc4ac8cf54c86785765
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2024-04-16 23:38:04 +0300

    calendar collection view + SnapKit integration

--- Weekler/Scenes/Schedule/View/ScheduleViewController.swift
+++ Weekler/Scenes/Schedule/View/ScheduleViewController.swift
@@ -6,13 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
 
 final class ScheduleViewController: UIViewController {
+    private let reuseId = "calendarCell"
+    
+    private lazy var backCalendarButton: UIButton = {
+        let button = UIButton()
+        
+        let image = UIImage(systemName: "chevron.backward")
+        button.setImage(image, for: .normal)
+        button.tintColor = .black
+        button.contentVerticalAlignment = .center
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        
+        return button
+    }()
+    private lazy var forwardCalendarButton: UIButton = {
+        let button = UIButton()
+        
+        let image = UIImage(systemName: "chevron.forward")
+        button.setImage(image, for: .normal)
+        button.tintColor = .black
+        button.contentVerticalAlignment = .center
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapForwardButton), for: .touchUpInside)
+        
+        return button
+    }()
+    private lazy var calendarCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .clear
+        collection.isScrollEnabled = true
+        collection.allowsSelection = true
+        collection.allowsMultipleSelection = false
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collection
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        
+        
+    }
+    
+    private func setupUI() {
         self.view.backgroundColor = Colors.background
         
         if let navBar = navigationController?.navigationBar {
@@ -20,5 +66,84 @@ final class ScheduleViewController: UIViewController {
             navigationItem.title = "Current week"
             navBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .medium)]
         }
+        
+        calendarCollectionView.register(CalendarCollectionViewCell.self, forCellWithReuseIdentifier: reuseId)
+        calendarCollectionView.dataSource = self
+        calendarCollectionView.delegate = self
+        
+        addSubviews()
+        applyConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(backCalendarButton)
+        view.addSubview(forwardCalendarButton)
+        view.addSubview(calendarCollectionView)
+    }
+    
+    private func applyConstraints() {
+        //back calendar button constraints
+        backCalendarButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.centerY.equalTo(calendarCollectionView.snp.centerY)
+            $0.width.equalTo(20)
+        }
+        
+        //forward calendar bitton constraints
+        forwardCalendarButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.centerY.equalTo(calendarCollectionView.snp.centerY)
+            $0.width.equalTo(20)
+        }
+        
+        //calendar collection view constraints
+        calendarCollectionView.snp.makeConstraints {
+            $0.leading.equalTo(backCalendarButton.snp.trailing)
+            $0.trailing.equalTo(forwardCalendarButton.snp.leading)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            $0.height.equalTo(70)
+        }
+    }
+    
+    @objc private func didTapBackButton() {
+        print("BACK")
+    }
+    
+    @objc private func didTapForwardButton() {
+        print("FORWARD")
+    }
+}
+
+extension ScheduleViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = calendarCollectionView.dequeueReusableCell(
+            withReuseIdentifier: reuseId,
+            for: indexPath) as? CalendarCollectionViewCell else 
+        {
+            assertionFailure("error while creating calendar cell")
+            return UICollectionViewCell()
+        }
+        
+        cell.configureCell()
+        
+        return cell
+    }
+}
+
+extension ScheduleViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+       return CGSize(width: 35, height: 60)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 }
