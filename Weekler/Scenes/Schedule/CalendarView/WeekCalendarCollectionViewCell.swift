3e0e7e42b5e81a25263389e3c2d871f9061a34ba
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2024-04-22 23:39:48 +0300

    dates selection and deselection with rounded red view highlighting

--- Weekler/Scenes/Schedule/CalendarView/WeekCalendarCollectionViewCell.swift
+++ Weekler/Scenes/Schedule/CalendarView/WeekCalendarCollectionViewCell.swift
@@ -9,12 +9,26 @@ import UIKit
 import JTAppleCalendar
 
 class WeekCalendarCollectionViewCell: JTAppleCell {
-    lazy var dateLabel: UILabel = {
+    private lazy var dateLabel: UILabel = {
         let label = UILabel()
         
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
+    }()
+    private lazy var selectedStateView: UIView = {
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = false
+        view.clipsToBounds = true
+//        view.layer.borderWidth = 1
+//        view.layer.cornerRadius = 20
+        view.backgroundColor = .clear
+        
+        
+        return view
     }()
     
     
@@ -30,6 +44,8 @@ class WeekCalendarCollectionViewCell: JTAppleCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        selectedStateView.layer.cornerRadius = CGRectGetHeight(selectedStateView.bounds) / 2.0
+//        selectedStateView.layer.cornerRadius = 25
     }
     
     override func prepareForReuse() {
@@ -45,19 +61,37 @@ class WeekCalendarCollectionViewCell: JTAppleCell {
     }
     
     private func addSubviews() {
+        contentView.addSubview(selectedStateView)
         contentView.addSubview(dateLabel)
     }
     
     private func applyConstraints() {
+        //date label constraints
         dateLabel.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.bottom.equalToSuperview()
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
         }
+        
+        //selected state view constraints
+        selectedStateView.snp.makeConstraints {
+//            $0.top.equalToSuperview()
+//            $0.bottom.equalToSuperview()
+//            $0.leading.equalToSuperview()
+//            $0.trailing.equalToSuperview()
+            $0.centerY.equalTo(contentView.snp.centerY)
+            $0.centerX.equalTo(contentView.snp.centerX)
+            $0.height.equalTo(30)
+            $0.width.equalTo(30)
+        }
     }
     
-    func configureCell() {
-        
+    func configureCell(with currentDate: String) {
+        dateLabel.text = currentDate
+    }
+    
+    func changeSelectionState(isSelected: Bool) {
+        selectedStateView.backgroundColor = isSelected ? .red : .clear
     }
 }
