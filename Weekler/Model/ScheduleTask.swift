b8249ba8f0bb25a088aa50b7896bcc2b5456054c
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2024-06-09 20:35:14 +0300

    Implement CreateScheduleItemsTableView

---
+++ Weekler/Model/ScheduleTask.swift
@@ -0,0 +1,13 @@
+//
+//  ScheduleTask.swift
+//  Weekler
+//
+//  Created by Eugene Kolesnikov on 06.06.2024.
+//
+
+import Foundation
+
+struct ScheduleTask {
+    let date: Date
+    let description: String
+}
